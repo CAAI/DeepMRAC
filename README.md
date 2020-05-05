2d5286861fe905d43cc43bbecd678f357651ff9a
Claes Ladefoged <claesnl@gmail.com>
2020-05-05 20:23:12 +0200

    Added install script

--- README.md
+++ README.md
@@ -8,7 +8,27 @@ both implemented for VB20P and VE11P in seperate models.
 
 Future release will also include T1w-MPRAGE. 
 
-## Contact: 
+## Requirements
+First, if you need to use GPU, you need to install the GPU, CUDA and cuDNN toolkits. See https://www.tensorflow.org/install/gpu.
+
+To install the required software tools, run:
+``
+pip3 install -r requirements.txt
+``
+
+Please note - to use DeepDixon, you further need to install FSL.
+
+The best performance will be obtained with GPU support (about 4 seconds, depending on GPU type), but CPU can also be used. Please install tensorflow rather than tensorflow-gpu (in requirements). The running time is about 15-20 minutes for CPU.
+
+## Running the script
+Change the root folder in the main script (on line 23) to where you put the main script.
+Run: python3 process_DeepUTE.py ﹤path to DICOM data﹥
+
+The output will be a folder called DeepUTE/DeepDixon within the DICOM data folder.
+
+## Contact
+Claes Ladefoged, Rigshospitalet, Copenhagen, Denmark
 claes.noehr.ladefoged@regionh.dk
 
-
+## Citation
+The publication has been submitted for review - please contact claes.noehr.ladefoged@regionh.dk for details on citations in the mean time
---
+++ install.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import wget
+from pathlib import Path
+import zipfile
+from shutil import copyfile, copymode
+import os
+
+# Setup folders
+if not os.path.exists('/opt/caai/share'):
+	print("Setting up install directories at /opt/caai")
+	Path('/opt/caai/share').mkdir(parents=True, exist_ok=True)
+	Path('/opt/caai/bin').mkdir(parents=True, exist_ok=True)
+
+# Download models
+if not os.path.exists('models.zip'):
+	print('Downloading models for DeepMRAC')
+	url = "http://resolute.pet.rh.dk:8000/models.zip"
+	wget.download(url,'models.zip')
+	print("")
+
+# Unzip models
+if not os.path.exists('/opt/caai/share/DeepMRAC'):
+	print("Extracting models")
+	with zipfile.ZipFile('models.zip', 'r') as zip_ref:
+		zip_ref.extractall('/opt/caai/share/DeepMRAC')
+
+# Install scripts
+if not os.path.exists('/opt/caai/bin/process_DeepDixon.py'):
+	print("Installing run scripts")	
+	copyfile('scripts/process_DeepDixon.py', '/opt/caai/bin/process_DeepDixon.py')
+	copyfile('scripts/process_DeepUTE.py', '/opt/caai/bin/process_DeepUTE.py')
+	copymode('scripts/process_DeepDixon.py', '/opt/caai/bin/process_DeepDixon.py')
+	copymode('scripts/process_DeepUTE.py', '/opt/caai/bin/process_DeepUTE.py')
+
